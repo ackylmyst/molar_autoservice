41212e37d792fe2bf4d4f169db654d2dd747955c
pallasov <ncrocat@gmail.com>
2026-04-21 17:24:08 +0700

    ссылка на эдит

--- autoservice/urls.py
+++ autoservice/urls.py
@@ -21,7 +21,7 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.orders_page, name='home'),
-    path('orders/', views.orders_page, name='home'),
+    path('edit/<int:order_id>/', views.edit_order, name='edit_order'),
     path('get-client-by-phone/', views.get_client_by_phone, name='get_client_by_phone'),
     path('get-car-by-plate/', views.get_car_by_plate, name='get_car_by_plate'),
     path('check-plate-owner/', views.check_plate_owner, name='check_plate_owner'),
