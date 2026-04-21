4d744497b2c75bbb0fdc74c2910f0d22fd90c824
pallasov <ncrocat@gmail.com>
2026-04-21 16:57:46 +0700

    заменены настройки дб под постгрес
    имя: postgres
    пароль: 1111

--- autoservice/settings.py
+++ autoservice/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'autoservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'autoservice',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
 
