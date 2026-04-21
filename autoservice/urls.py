413558325f8c43a24ba4d1642f0e8229bb8bfd1f
pallasov <ncrocat@gmail.com>
2026-04-21 16:58:05 +0700

    Update urls.py

--- autoservice/urls.py
+++ autoservice/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', views.home),
-    path('requests/', views.requests_page),
-    
+    path('', views.orders_page, name='home'),
+    path('orders/', views.orders_page, name='home'),
+    path('get-client-by-phone/', views.get_client_by_phone, name='get_client_by_phone'),
+    path('get-car-by-plate/', views.get_car_by_plate, name='get_car_by_plate'),
+    path('check-plate-owner/', views.check_plate_owner, name='check_plate_owner'),
 ]
